eca5866be5892d82c5c6116b8415494b0a1721ae
KunalThakur-Dev <kunalt1619@gmail.com>
2026-06-09 13:33:18 +0530

    Add files via upload

---
+++ 01_info.js
@@ -0,0 +1,12 @@
+// Personal Information
+
+let name = "Kunal Kumar";
+let age = 19;
+let city = "Garhmuteshar";
+
+let isStudent = true;
+
+console.log(`My name is ${name}, I am ${age} years old and I live in ${city}`);
+
+//Expected Output - My name is Kunal, I am 19 years old and I live in Garhmukteshwar.
+
